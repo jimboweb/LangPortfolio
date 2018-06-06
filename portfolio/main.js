836744687994fce44e5f1eb28b7ee9ddce532d4d
jimboweb <jimboweb@users.noreply.github.com>
2018-06-06 14:35:02 -0400

    added slideshow method

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,3 +1,5 @@
+const slideInterval = 2;
+
 $(document).ready(()=>{
     start();
 })
@@ -5,7 +7,6 @@ $(document).ready(()=>{
 
 
 const start = () => {
-    console.log("started");
     const jqxhr = $.getJSON( "./links.json", (json) => {
         const links = json["imageLinks"];
         const htm = links.map((item, i, links)=>{
@@ -17,5 +18,28 @@ const start = () => {
     })
     .fail(()=>{
         console.log("not loaded");
+    })
+    .done(()=>{
+        slideShow();
     });
+}
+
+const slideShow = () => {
+    doSwitchSlide();
+    setInterval(doSwitchSlide, slideInterval*1000);
+}
+
+const doSwitchSlide = () => { //impure
+    const slideDiv = $("#slideshow");
+    const nextTnDiv = parseInt(slideDiv.attr("data-next"));
+    const thumbnails = $("#thumbnails").children();
+    const nextTn = thumbnails.eq(nextTnDiv);
+    const imgSrc = nextTn.find("img").attr("src");
+    const caption = nextTn.find("figcaption").text();
+    const link = nextTn.find("a").attr("href")
+    $("img#slideImage").attr("src", imgSrc);
+    $("figcaption#slideCaption").html(caption);
+    $("a#slideLink").attr("href",link);
+    const newDiv = (nextTnDiv+1)%thumbnails.length;
+    slideDiv.attr("data-next", newDiv);
 }
