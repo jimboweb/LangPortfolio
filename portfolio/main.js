3797a920d2e66f4386c22ea1675f5ca09356ef41
jimboweb <jimboweb@users.noreply.github.com>
2018-06-06 09:52:53 -0400

    flex grid working

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,15 +1,21 @@
 $(document).ready(()=>{
     start();
 })
+
+
+
 const start = () => {
     console.log("started");
     const jqxhr = $.getJSON( "./links.json", (json) => {
         const links = json["imageLinks"];
-        links.map((item, i, links)=>{
-            console.log(item["image"]);
-        })
-   })
-        .fail(()=>{
-            console.log("not loaded");
-        });
+        const htm = links.map((item, i, links)=>{
+            return $.templates("#thumbnailTmpl").render(links); // Get compiled template
+        }).join("");
+        const tn = $("#thumbnails").html();
+        $("#thumbnails").html(htm);           // Insert HTML string into DOM
+
+    })
+    .fail(()=>{
+        console.log("not loaded");
+    });
 }
