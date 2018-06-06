11d3a46c98f792bc728405fd482031dce568439a
jimboweb <jimboweb@users.noreply.github.com>
2018-06-06 17:57:04 -0400

    student work added

--- portfolio/main.js
+++ portfolio/main.js
@@ -9,10 +9,7 @@ $(document).ready(()=>{
 const start = () => {
     const jqxhr = $.getJSON( "./links.json", (json) => {
         const links = json["imageLinks"];
-        const htm = links.map((item, i, links)=>{
-            return $.templates("#thumbnailTmpl").render(links); // Get compiled template
-        }).join("");
-        const tn = $("#thumbnails").html();
+        const htm = $.templates("#thumbnailTmpl").render(links);
         $("#thumbnails").html(htm);           // Insert HTML string into DOM
 
     })
